c5fb6ec39ac63080dd3593cbac897f90eb05dc0f
Francisco Coimbra Rotilli <francisco.r@edu.pucrs.br>
2024-09-25 12:53:45 -0300

    Aula 2

---
+++ src/componentes/ManuLink/index.js
@@ -0,0 +1,18 @@
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import styles from './MenuLink.module.css'
+
+export default function MenuLink({ children, to }) {
+    const localizacao = useLocation()
+
+    return (
+        <NavLink
+            to={to}
+            className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.linkDestacado : ''}
+            `}
+        >
+            {children}
+        </NavLink>
+    )
+}
